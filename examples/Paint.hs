74f44c4922b364f77c0948eb807b6594c7eeaf1e
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2014-10-22 00:16:24 -0400

    Paint example uses SDL, is still silly

--- examples/Paint.hs
+++ examples/Paint.hs
@@ -1,16 +1,33 @@
+-- It's a mess in here. We use SDL to make a silly drawing app. Click around
+-- to add points, and use z and y to undo/redo, any other key will quit.
+-- NB commands add _points_ not lines, so if you undo all the way, it will
+-- seem like the first redo does nothing, but in fact it adds a point.
+
+import Prelude hiding (init)
+
 import qualified Data.Set as S
 import qualified Data.Map as M
-import Data.Ratio
+import Data.String
+import Data.Traversable
 
--- Random point.
-import System.Random
-import Debug.Trace
+import Graphics.UI.SDL.Basic
+import Graphics.UI.SDL.Enum
+import Graphics.UI.SDL.Types hiding (Point)
+import Graphics.UI.SDL.Event
+import Graphics.UI.SDL.Video
 
 import Control.CommandRunner
+import Control.Applicative
+import Control.Monad
+
+import Foreign.C.String
+import qualified Foreign.Storable as FS
+import Foreign.Marshal.Alloc
+
+import Debug.Trace
 
 data Colour = Red | Green | Blue
--- We use Ratio because we want reliable equality.
-type Point = (Rational, Rational)
+type Point = (Int, Int)
 type PointListIdentifier = Int
 type PointList = [Point]
 type PointLists = M.Map PointListIdentifier PointList
@@ -55,29 +72,64 @@ removePoint pid point = Command $ do
   modify (removePoint' pid point)
   return $ addPoint pid point
 
+-- Here we see how Haskell shines as the world's best imperative programming
+-- language.
 main = do
   cmdRunner <- newCommandRunner emptyDrawingState
-  mainLoop cmdRunner
+  status <- fromIntegral <$> init initFlagEverything
+  unless (status == 0) (mainSDL cmdRunner)
+  quit
 
-mainLoop cmdRunner = do
-  putChar '>'
-  putChar ' '
-  c <- getChar
-  putChar '\n'
-  case c of
-    'u' -> undo cmdRunner (const $ return ())
-    'r' -> redo cmdRunner (const $ return ())
-    'p' -> peek cmdRunner >>= print
-    'n' -> do
-        gen <- newStdGen
-        let (randomId, gen1) = randomR (1, 100) gen
-        let (randomNumerator1, gen2) = random gen1
-        let (randomDenominator1, gen3) = random gen2
-        let (randomNumerator2, gen4) = random gen3
-        let (randomDenominator2, gen5) = random gen4
-        let randomPoint = (randomNumerator1 % randomDenominator1, randomNumerator2 % randomDenominator2)
-        print randomId
-        print randomPoint
-        exec cmdRunner (const $ return ()) (addPoint randomId randomPoint)
-    _ -> return ()
-  mainLoop cmdRunner
+mainSDL cmdRunner = do
+  window <- withCString "Paint" (\s -> createWindow s (fromIntegral (0 :: Int)) (fromIntegral (0 :: Int)) (fromIntegral (640 :: Int)) (fromIntegral (480 :: Int)) windowFlagShown)
+  renderer <- createRenderer window (fromIntegral ((-1) :: Int)) rendererFlagAccelerated
+  mainLoop cmdRunner renderer
+  destroyRenderer renderer
+  destroyWindow window
+
+mainLoop cmdRunner renderer = do
+  -- Start by clearing
+  setRenderDrawColor renderer (fromIntegral 0) (fromIntegral 0) (fromIntegral 0) (fromIntegral 255)
+  renderClear renderer
+  -- Draw all lines
+  state <- peek cmdRunner
+  setRenderDrawColor renderer (fromIntegral 255) (fromIntegral 255) (fromIntegral 255) (fromIntegral 255)
+  drawLines renderer state
+  renderPresent renderer
+  -- Wait for input.
+  waitForHandledEvent cmdRunner renderer
+
+waitForHandledEvent cmdRunner renderer = alloca $ \eventPtr -> do
+  waitEvent eventPtr
+  ev <- FS.peek eventPtr
+  case ev of
+    k@(KeyboardEvent _ _ _ _ _ _) -> handleKeyboardEvent cmdRunner renderer k
+    m@(MouseButtonEvent _ _ _ _ _ _ _ _ _) -> handleMouseEvent cmdRunner renderer m
+    _ -> waitForHandledEvent cmdRunner renderer
+
+handleKeyboardEvent cmdRunner renderer kb = do
+  let code = (keysymKeycode . keyboardEventKeysym) kb
+  if code == keycodeZ
+  then undo cmdRunner k
+  else if code == keycodeY
+    then redo cmdRunner k
+    -- Any other key will quit!
+    else return ()
+  where k = \_ -> mainLoop cmdRunner renderer
+
+handleMouseEvent cmdRunner renderer mb = do
+  exec cmdRunner k (addPoint 0 (x, y))
+    where x = fromIntegral $ mouseButtonEventX mb
+          y = fromIntegral $ mouseButtonEventY mb
+          k = \_ -> mainLoop cmdRunner renderer
+
+drawLines renderer ds = traverse (drawPointList renderer) (_pointLists ds) >> return ()
+
+drawPointList renderer pl = mapM_ (sdlRenderLine renderer) pairs
+  where pairs = pl `zip` (tail pl)
+
+sdlRenderLine renderer (p, q) = renderDrawLine renderer x0 y0 x1 y1 >> return ()
+  where x0 = fromIntegral . fst $ p
+        y0 = fromIntegral . snd $ p
+        x1 = fromIntegral . fst $ q
+        y1 = fromIntegral . snd $ q
